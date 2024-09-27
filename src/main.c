8b2b910659ca459a904e6d09678b3c45a6fedb2a
Jon Johnson <jonjohnsontc@live.com>
2024-09-26 19:22:51 -0700

    checkpoint

--- src/main.c
+++ src/main.c
@@ -16,30 +16,32 @@
    Note: The result is an int instead of a pointer to the next byte to be
    read to avoid const-vs-non-const problems.  */
 static inline size_t
-read_sleb128_to_int64 (const unsigned char *buf, const unsigned char *buf_end,
-		       int64_t *r)
-{
-  const unsigned char *p = buf;
-  unsigned int shift = 0;
-  int64_t result = 0;
-  unsigned char byte;
+read_sleb128_to_int64(const unsigned char *buf, const unsigned char *buf_end,
+                      int64_t *r) {
+    const unsigned char *p = buf;
+    unsigned int shift = 0;
+    int64_t result = 0;
+    unsigned char byte;
 
-  while (1)
-    {
-      if (p >= buf_end)
-	return 0;
+    while (1) {
+        if (p >= buf_end)
+            return 0;
 
-      byte = *p++;
-      result |= ((uint64_t) (byte & 0x7f)) << shift;
-      shift += 7;
-      if ((byte & 0x80) == 0)
-	break;
+        byte = *p++;
+        result |= ((uint64_t) (byte & 0x7f)) << shift;
+        shift += 7;
+        if ((byte & 0x80) == 0)
+            break;
     }
-  if (shift < (sizeof (*r) * 8) && (byte & 0x40) != 0)
-    result |= -(((uint64_t) 1) << shift);
+    if (shift < (sizeof(*r) * 8) && (byte & 0x40) != 0)
+        result |= -(((uint64_t) 1) << shift);
 
-  *r = result;
-  return p - buf;
+    *r = result;
+    return p - buf;
+}
+
+void read_footer(const char *footer, size_t length) {
+
 }
 
 /*
@@ -48,50 +50,69 @@ read_sleb128_to_int64 (const unsigned char *buf, const unsigned char *buf_end,
   -
 */
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    fprintf(stderr, "USAGE: tiny-p FILENAME\n");
-    exit(1);
-  }
+    if (argc != 2) {
+        fprintf(stderr, "USAGE: tiny-p FILENAME\n");
+        exit(1);
+    }
 
-  FILE* parquet = fopen(argv[1], "rb");
-  if (parquet == NULL) {
-    perror("Error opening file");
-    exit(1);
-  }
+    FILE *parquet = fopen(argv[1], "rb");
+    if (parquet == NULL) {
+        perror("Error opening file");
+        exit(1);
+    }
 
-  fseek(parquet, 0, SEEK_END);
-  long file_size = ftell(parquet);
-  if (file_size < 8) {
-    fprintf(stderr, "File is too small - def not a proper parquet file\n");
+    fseek(parquet, 0, SEEK_END);
+    long file_size = ftell(parquet);
+    if (file_size < 8) {
+        fprintf(stderr, "File is too small - def not a proper parquet file\n");
+        fclose(parquet);
+        exit(1);
+    }
+
+    // Go to the location of the length of the metadata file
+    fseek(parquet, -8, SEEK_END);
+
+    uint8_t buffer[4];
+    if (fread(buffer, 1, 4, parquet) != 4) {
+        perror("Error reading file");
+        fclose(parquet);
+        exit(1);
+    }
+
+
     fclose(parquet);
-    exit(1);
-  }
-  
-  // Go to the location of the length of the metadata file
-  fseek(parquet, -8, SEEK_END);
 
-  uint8_t buffer[4];
-  if (fread(buffer, 1, 4, parquet) != 4) {
-    perror("Error reading file");
-    fclose(parquet);
-    exit(1);
-  }
+    uint32_t result = (uint32_t) buffer[0] |
+                      ((uint32_t) buffer[1] << 8) |
+                      ((uint32_t) buffer[2] << 16) |
+                      ((uint32_t) buffer[3] << 24);
+    printf("Length of metadata is %d bytes\n", result);
 
-  fclose(parquet);
+    //  We'll copy and invoke the read_footer function
+    fseek(parquet, -(result+4), SEEK_CUR);
 
-  uint32_t result = (uint32_t)buffer[0] |
-                    ((uint32_t)buffer[1] << 8) |
-                    ((uint32_t)buffer[2] << 16) |
-                    ((uint32_t)buffer[3] << 24);
-  printf("Length of metadata is %d bytes\n", result);
-  printf("Size of int64_t is %ld bytes\n", sizeof(int64_t));
-  printf("Size of long is %ld bytes\n", sizeof(long));
+    char* meta = malloc(sizeof(char) * result);
+    if (meta == NULL) {
+        fprintf(stderr, "malloc error\n");
+        exit(1);
+    }
+    if (fread(meta, 1, result, parquet) != result) {
+        perror("Error reading file");
+        fclose(parquet);
+        exit(1);
+    }
+    printf("%s\n", meta);
 
-  printf("Parsing zigzag encoded bytestring\n");
-  const unsigned char bytes[] = { 0xC0, 0xBB, 0x78 };
-  const unsigned char* end = bytes + 4;
-  int64_t res = 0;
-  read_sleb128_to_int64(bytes, end, &res);
-  printf("bytes equal %ld\n", res);
-  return 0;
+    printf("Size of int64_t is %ld bytes\n", sizeof(int64_t));
+    printf("Size of long is %ld bytes\n", sizeof(long));
+
+    printf("Parsing zigzag encoded bytestring\n");
+    const unsigned char bytes[] = {0xC0, 0xBB, 0x78};
+    const unsigned char *end = bytes + 4;
+    int64_t res = 0;
+    read_sleb128_to_int64(bytes, end, &res);
+    printf("bytes equal %lld\n", res);
+
+    free(meta);
+    return 0;
 }
